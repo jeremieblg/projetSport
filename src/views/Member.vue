55d8826467adf6f56dfc461ff8829be969b1f349
jeremieblg <bellanger.jeremie@gmail.com>
2020-07-03 12:06:48 +0200

    member from correction

--- src/views/Member.vue
+++ src/views/Member.vue
@@ -12,68 +12,61 @@
           min-width="290px"
         >
           <template v-slot:activator="{ on, attrs }">
-            <v-text-field
-              v-model="date"
-              label="Picker without buttons"
-              readonly
-              v-bind="attrs"
-              v-on="on"
-            ></v-text-field>
+            <v-text-field v-model="date" label="Date" readonly v-bind="attrs" v-on="on"></v-text-field>
           </template>
           <v-date-picker v-model="date" @input="showDatePicker = false"></v-date-picker>
         </v-menu>
       </v-row>
-      <v-text-field v-model="name" :counter="10" :rules="nameRules" label="Sport" required></v-text-field>
+      <v-text-field
+        v-model="sport"
+        :counter="30"
+        label="Sport"
+        :rules="[v => !!v || 'Champ requis']"
+        required
+      ></v-text-field>
 
       <v-text-field
-        v-model="email"
-        :rules="emailRules"
+        v-model="duration"
         label="Durée en minutes"
+        :rules="[v => !!v || 'Champ requis']"
         type="number"
         required
       ></v-text-field>
       <v-select
-        v-model="select"
-        :items="items"
-        :rules="[v => !!v || 'Item is required']"
+        v-model="intensity"
+        :items="intensityTab"
+        :rules="[v => !!v || 'Champ requis']"
         label="Intensité"
         required
       ></v-select>
       <v-select
-        v-model="select"
-        :items="items"
-        :rules="[v => !!v || 'Item is required']"
+        v-model="breakfast"
+        :items="lunchWeight"
+        :rules="[v => !!v || 'Champ requis']"
         label="Petit déjeuner"
         required
       ></v-select>
       <v-select
-        v-model="select"
-        :items="items"
-        :rules="[v => !!v || 'Item is required']"
+        v-model="lunch"
+        :items="lunchWeight"
+        :rules="[v => !!v || 'Champ requis']"
         label="Déjeuner"
         required
       ></v-select>
       <v-select
-        v-model="select"
-        :items="items"
-        :rules="[v => !!v || 'Item is required']"
+        v-model="dinner"
+        :items="lunchWeight"
+        :rules="[v => !!v || 'Champ requis']"
         label="Diner"
         required
       ></v-select>
 
-      <v-checkbox
-        v-model="checkbox"
-        :rules="[v => !!v || 'You must agree to continue!']"
-        label="Collation"
-        required
-      ></v-checkbox>
-      <v-text-field v-model="email" :rules="emailRules" label="Poid" type="number" required></v-text-field>
+      <v-checkbox v-model="snack" label="Collation" required></v-checkbox>
+      <v-text-field v-model="weight" label="Poid" type="number" required></v-text-field>
 
       <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">Validate</v-btn>
 
-      <v-btn color="error" class="mr-4" @click="reset">Reset Form</v-btn>
-
-      <v-btn color="warning" @click="resetValidation">Reset Validation</v-btn>
+      <v-btn color="error" class="mr-4" @click="reset">Reset Formulaire</v-btn>
     </v-form>
   </v-container>
 </template>
@@ -84,11 +77,30 @@ export default {
     return {
       date: new Date().toISOString().substr(0, 10),
       showDatePicker: false,
-      picker: new Date().toISOString().substr(0, 10)
+      sport: "",
+      duration: "",
+      intensity: "",
+      intensityTab: ["faible", "normal", "forte"],
+      lunchWeight: ["léger", "normalement", "beaucoup"],
+      breakfast: "",
+      lunch: "",
+      dinner: "",
+      weight: "",
+      snack: false
     };
   },
   methods: {
-    openDatePicker() {}
+    validate() {},
+    reset() {
+      this.sport = "";
+      this.duration = "";
+      this.intensity = "";
+      this.breakfast = "";
+      this.lunch = "";
+      this.dinner = "";
+      this.weight = "";
+      this.snack = false;
+    }
   }
 };
 </script>
