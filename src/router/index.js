f741be6753d56337488bcb62e4cb2ef6d6623bc6
jeremieblg <bellanger.jeremie@gmail.com>
2020-07-03 10:52:49 +0200

    add routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-
+import Connection from '../views/Connection.vue';
+import Admin from '../views/Admin.vue';
+import Coach from '../views/Coach.vue';
+import Member from '../views/Member.vue';
 Vue.use(VueRouter)
 
   const routes = [
@@ -9,6 +12,26 @@ Vue.use(VueRouter)
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/connection',
+    name: 'Connection',
+    component: Connection
+  },
+  {
+    path: '/admin',
+    name: 'Admin',
+    component: Admin
+  },
+  {
+    path: '/coach',
+    name: 'Coach',
+    component: Coach
+  },
+  {
+    path: '/member',
+    name: 'Member',
+    component: Member
   },
   {
     path: '/about',
