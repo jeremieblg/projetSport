f36563851d6ab7e6fced6a44f5cd29dd096fae37
Jeremy Diot <diot.jeremy@gmail.com>
2020-07-03 12:42:41 +0200

    connection vue

--- src/views/Connection.vue
+++ src/views/Connection.vue
@@ -1,5 +1,42 @@
 <template>
-  <div>connection</div>
+  <div>
+    <v-container>
+      <h1>Se connecter</h1>
+            <v-form class="mb-10">
+        <v-row>
+          <v-text-field
+            v-model="email"
+            type="email"
+            label="E-mail"
+            required
+            clearable
+          ></v-text-field>
+        </v-row>
+        <v-row>
+          <v-text-field
+            v-model="password"
+            type="password"
+            label="Mot de passe"
+            required
+            clearable
+          ></v-text-field>
+        </v-row>
+        <v-row>
+          <v-btn
+            dark
+            class="mr-4"
+            @click="send"
+          >
+            Se connecter
+          </v-btn>
+        </v-row>
+      </v-form>
+    </v-container>
+  </div>
+  
+
+  
+
 </template>
 
 <script>
