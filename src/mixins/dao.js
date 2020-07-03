bdb019327e259d921a98ecbd31a1228cabd0b7ae
Jeremy Diot <diot.jeremy@gmail.com>
2020-07-03 14:08:34 +0200

    partialy implement axios mixin

---
+++ src/mixins/dao.js
@@ -0,0 +1,31 @@
+const URL = process.env.VUE_APP_URL_API;
+
+export default{
+    methods: {
+        async userExist(email,password){
+            const user = await vue.axios.get(URL+"/users",{password:password,email:email});
+            return (user[0])?user[0]:undefined;
+        },
+        async adminExist(email,password){
+            const admin = await vue.axios.get(URL+"/admin",{password:password,email:email});
+            return (admin[0])?admin[0]:undefined;
+        },
+        async getUsers(){
+            const users = await vue.axios.get(URL+"/users");
+            return users;
+        },
+        async getUser(id){
+            const user = await vue.axios.get(URL+"/users",{id:id});
+            return (user[0])?user[0]:undefined;
+        },
+        async addUser(userObject){
+
+        },
+        async addStatement(userId,statementObject){
+
+        },
+        async addMessage(userId, message){
+
+        }
+    }
+}
